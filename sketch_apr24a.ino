a9a2210b524eaaf343079e273722d8821b1cf4ac
Andrés Manrique <110911279+Tdristados@users.noreply.github.com>
2024-04-25 04:33:32 -0500

    Update sketch_apr24a.ino
    Adicionamiento de la función protocoloReversa, para evitar que el Sumo se salga de la pista

--- sketch_apr24a.ino
+++ sketch_apr24a.ino
@@ -22,9 +22,10 @@ const int INIT = 13;
 #define PWMR 4   // Potencia del motor derecho (valores entre 0-255)
 #define RDIR 3   // Direccionamiento del motor derecho
 
-// Supondremos que el Pin de direccionamieno en 1 hará que el motor
-// vaya en sentido horario
-
+int velmax = 255;
+int velizq = 255;
+int velder = 255;
+int n = 0;
 
 void setup() {
   Serial.begin(9600); // Inicia el puerto serial
@@ -47,84 +48,91 @@ void setup() {
 }
 
 void loop() {
-  // Función para medir la distancia
-  int d1 = getDistance(trigPin1, echoPin1);
-  int d2 = getDistance(trigPin1, echoPin2);
-  int d3 = getDistance(trigPin1, echoPin3);
-  int d4 = getDistance(trigPin1, echoPin4);
+  // INIT para inicializar 
+  int inicializacion = digitalRead(INIT);
   
-  int sensorValue1 = digitalRead(SLL);
-  int sensorValue2 = digitalRead(SLR);
+//while (inicialitation){
+
+    // Función para medir la distancia
+    int d1 = getDistance(trigPin1, echoPin1);
+    int d2 = getDistance(trigPin1, echoPin2);
+    int d3 = getDistance(trigPin1, echoPin3);
+    int d4 = getDistance(trigPin1, echoPin4);
+    
+    int sensorValue1 = digitalRead(SLL);
+    int sensorValue2 = digitalRead(SLR);
 
 
-  if(d1==0)
-  {
-    if(d2==0)
+    if(d1==0)
     {
-       if(d3==0)
-       {
-        if(d4==0)
-        { //1
-        Serial.print("caso 1");
+      if(d2==0)
+      {
+        if(d3==0)
+        {
+          if(d4==0)
+          { //1
+          Serial.print("caso 1");
+          }
+          else
+          { //2
+            Serial.print("caso 2");
+          }
         }
         else
-        { //2
-          Serial.print("caso 2");
+        { 
+          if(d4==0)
+          { //3
+            Serial.print("caso 3");
+          }
+          else
+          { //4
+            Serial.print("caso 4");
+          }
         }
-       }
-       else
-       { 
-        if(d4==0)
-        { //3
-          Serial.print("caso 3");
+      }else{
+        if(d3==0){
+          if(d4==0){ //5
+          Serial.print("caso 5");
+          }
+        }else{
+          if(d4==0){ //7
+            Serial.print("caso 7");
+          }
         }
-        else
-        { //4
-          Serial.print("caso 4");
-        }
-       }
-    }else{
-      if(d3==0){
-        if(d4==0){ //5
-        Serial.print("caso 5");
-        }
-       }else{
-        if(d4==0){ //7
-          Serial.print("caso 7");
-        }
-       }
-    }
-  }
-  else
-  {
-    if(d2==0)
-    {
-       if(d3==0)
-       {
-        if(d4==0)
-        { //9
-          Serial.print("caso 9");
-        }
-       }
-       else
-       {
-       }
+      }
     }
     else
     {
-      if(d3==0)
+      if(d2==0)
       {
-        if(d4==0)
-        { //13
-          Serial.print("caso 13");
+        if(d3==0)
+        {
+          if(d4==0)
+          { //9
+            Serial.print("caso 9");
+          }
         }
-       }
-       else
-       {
-       }
+        else
+        {
+        }
+      }
+      else
+      {
+        if(d3==0)
+        {
+          if(d4==0)
+          { //13
+            Serial.print("caso 13");
+          }
+        }
+        else
+        {
+        }
+      }
     }
   }
-}
+
+//}
 
 /*------------ Funciones-------*/
 
@@ -171,13 +179,13 @@ void driveMotor(int motor, int pwm, int dir){
 }
 
 void avanzar(int pwm){
-  pwm = pwm>=255? 255:pwm; // Si de casualidad el valor se excede, se corrige
+  pwm = pwm>=velmax? velmax:pwm; // Si de casualidad el valor se excede, se corrige
   driveMotor(PWML,pwm,1); // El motor izquierdo va hacía adelante con una potencia de pwm
   driveMotor(PWMR,pwm,1); // El motor derecho  va hacía adelante con una potencia de pwm
 }
 
 void retroceder(int pwm){
-  pwm = pwm>=255? 255:pwm; 
+  pwm = pwm>=velmax? velmax:pwm; 
   driveMotor(PWML,pwm,0); 
   driveMotor(PWMR,pwm,0); 
 }
@@ -185,8 +193,8 @@ void retroceder(int pwm){
 // Gira pero es necesario saber qué pwms toma cada valor, yendo siempre hacía adelante ambos.
 // Así, es posible girar a la derecha (pwml>pwmr) o a izquierda (pwml<pwmr)
 void girar(int pwml, int pwmr){
-  pwml = pwml>=255? 255:pwml;
-  pwmr = pwmr>=255? 255:pwmr; 
+  pwml = pwml>=velmax? velmax:pwml;
+  pwmr = pwmr>=velmax? velmax:pwmr; 
   driveMotor(PWML,pwml,1);
   driveMotor(PWMR,pwmr,1);
 }
@@ -194,9 +202,9 @@ void girar(int pwml, int pwmr){
 // Se gira el robot de manera brusca, por lo tanto, una llanta va hacía el frente y la otra hacía atrás.
 // De nuevo, es necesario cada pwm para cada uno.
 // Si direccion = 1 el robot se voltea hacía la derecha, si es 0 hacía la izquierda 
-void voltear(int direccion, int pwml, int pwmr){
-  pwml = pwml>=255? 255:pwml;
-  pwmr = pwmr>=255? 255:pwmr;  
+void voltear(int direccion, int pwml, int pwmr){ //Hay que revisar cuánto gira
+  pwml = pwml>=velmax? velmax:pwml;
+  pwmr = pwmr>=velmax? velmax:pwmr;  
   if (direccion){ // si direccion = 1, entonces el motor izquierdo va hacia adelante y el derecho hacía atrás
     driveMotor(PWML,pwml,1);
     driveMotor(PWMR,pwmr,0);
@@ -204,4 +212,12 @@ void voltear(int direccion, int pwml, int pwmr){
     driveMotor(PWML,pwml,0);
     driveMotor(PWMR,pwmr,1);
   }
+}
+
+// Función, protocolo de seguridad para evitar que se salga el robot de la pista
+void protocoloReversa(){
+  // La idea es ir hacía atrás y dar vuelta para ir hacia el centro
+  retroceder(velmax);
+  delay(50);
+  voltear(1,velmax,velmax/2);  // que la llanta derecha vaya hacía atrás, pero con menos fuerza
 }
